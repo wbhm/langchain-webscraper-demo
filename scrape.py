89648368271384270a7e48489f43fae50c7ef526
wbhm <wbhm@mac.com>
2024-05-28 16:50:44 -0400

    updated scrape.py to leverage sitemap.xml.  archived old file

--- scrape.py
+++ scrape.py
@@ -1,79 +1,69 @@
 import argparse
-import requests
-import os
-from urllib.parse import urlparse
-from collections import defaultdict
-from bs4 import BeautifulSoup
 import json
+import os
+import re
+import ssl
+
+import requests
+import urllib.request
+from urllib.parse import urlparse, urljoin
+from urllib.error import URLError, HTTPError, ContentTooShortError
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--site", type=str, required=True)
-parser.add_argument("--depth", type=int, default=3)
-
+parser.add_argument("--retries", type=int, default=0)
 
 def cleanUrl(url: str):
     return url.replace("https://", "").replace("/", "-").replace(".", "_")
 
+def download_and_save(url, user_agent='webscraper', num_retires=0 , charset='utf-8'):
+    print ('Downloading', url)
 
-def get_response_and_save(url: str):
-    response=''
     try:
-        response = requests.get(url)
+        request = urllib.request.Request(url, headers={'User-Agent': user_agent})
+        request.add_header('User-Agent', user_agent)
+        gcontext = ssl.SSLContext()
+        response = urllib.request.urlopen(request, context=gcontext)
+
         if not os.path.exists("./scrape"):
             os.mkdir("./scrape")
         parsedUrl = cleanUrl(url)
+        cs = response.headers.get('char-set')
+        if not cs:
+            cs = charset
         with open("./scrape/" + parsedUrl + ".html", "w") as f:
             try:
-                f.write(response.content.decode("utf-8"))
-            except:
-                return response
-    finally:
-        return response
+                response_bytes = response.read()
+                html = response_bytes.decode(charset)
+                f.write(html)
+            except Exception as e:
+                print(e)
+                html = None
+    except (URLError, HTTPError, ContentTooShortError) as e:
+        print('Download Error: ', e.reason)
+        html = None
+        if num_retires > 0 :
+            if hasattr(e, 'code') and 500 <= e.code < 600 : # recursively retry 5XX HTTP Errors
+                return download_and_save(url, num_retires - 1)
+    return html
 
 
-def scrape_links(
-    scheme: str,
-    origin: str,
-    path: str,
-    depth=3,
-    sitemap: dict = defaultdict(lambda: ""),
-):
-    siteUrl = scheme + "://" + origin + path
-    cleanedUrl = cleanUrl(siteUrl)
-
-    if depth < 0:
-        return
-    if sitemap[cleanedUrl] != "":
-        return
-
-    sitemap[cleanedUrl] = siteUrl
-    response = get_response_and_save(siteUrl)
-    try:
-        if response:
-                soup = BeautifulSoup(response.content, "html.parser")
-                links = soup.find_all("a")
-
-                for link in links:
-                    href = urlparse(link.get("href"))
-                    if (href.netloc != origin and href.netloc != "") or (
-                        href.scheme != "" and href.scheme != "https"
-                    ):
-                        continue
-                    scrape_links(
-                        href.scheme or "https",
-                        href.netloc or origin,
-                        href.path,
-                        depth=depth - 1,
-                        sitemap=sitemap,
-                    )
-    finally:
-        return sitemap
-
+def crawl_sitemap(url):
+    # download site map xml
+    sitemap = requests.get(url)
+    #extract site mapp links
+    links = re.findall(r'<loc>(.+?)</loc>', sitemap.text)
+    # download each link
+    for link in links:
+        html = download_and_save(link, retries)
+        #scrape html
+    with open("./scrape/sitemap.xml", "w") as f:
+        f.write(sitemap)
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    url = urlparse(args.site)
-    print(url)
-    sitemap = scrape_links(url.scheme, url.netloc, url.path, depth=args.depth)
-    with open("./scrape/sitemap.json", "w") as f:
-        f.write(json.dumps(sitemap))
+    url = args.site
+    retries = args.retries
+    print('Crawling: ', url)
+    crawl_sitemap(url)
